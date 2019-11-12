0e23ff061af78ad635c674ba5ea6bfe517f83d9f
medhanafi <xskullbox@gmail.com>
2019-11-12 11:28:42 +0100

    Update config

--- elk/ubuntubootstrap.sh
+++ elk/ubuntubootstrap.sh
@@ -7,15 +7,32 @@ sudo apt-get update
 sudo apt-get install openjdk-8-jre-headless -y
 
 # install elasticsearch
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.7.2.deb
-sudo dpkg -i elasticsearch-6.7.2.deb
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.4.3.tar.gz
+tar -xzf elasticsearch-6.4.3.tar.gz
 
 # start elasticsearch on boot
-sudo update-rc.d elasticsearch defaults 95 10
+#sudo update-rc.d elasticsearch defaults 95 10
 
 # allow host OS to access through port forwarding
 sudo echo "
 network.bind_host: 0
-network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
-sudo sed -i -e '$a\' /etc/elasticsearch/elasticsearch.yml
-sudo sed -i -e '$a\' /etc/elasticsearch/elasticsearch.yml
+network.host: 0.0.0.0" >> /home/vagrant/elasticsearch-6.4.3/config/elasticsearch.yml
+sudo sed -i -e '$a\' /home/vagrant/elasticsearch-6.4.3/config/elasticsearch.yml
+sudo sed -i -e '$a\' /home/vagrant/elasticsearch-6.4.3/config/elasticsearch.yml
+sudo sed 's/-Xms1g/-Xms256m/g' /home/vagrant/elasticsearch-6.4.3/config/jvm.options
+sudo sed 's/-Xmx1g/-Xmx256m/g' /home/vagrant/elasticsearch-6.4.3/config/jvm.options
+
+#sudo vim /etc/sysctl.conf
+#vm.max_map_count=262144
+
+#Elastic yml config
+#node.name: dsc-node-1
+#cluster.name: dsc-cluster
+#network.host: 0.0.0.0
+#network.bind_host: 192.168.33.21
+#transport.tcp.port: 9300
+
+#JVM options
+#sudo vim config/jvm.options
+#-Xms512m
+#-Xmx512m
